a76c63e30b78fc27c1f5e78cf00725923be827a9
LaiaFerrerMartinez <laia.ferrer.martinez@gmail.com>
2024-10-04 19:32:34 +0200

    Simpson.java

--- 01 - Simpson/01 - Simpson/src/Simpson.java
+++ 01 - Simpson/01 - Simpson/src/Simpson.java
@@ -3,7 +3,7 @@ public class Simpson {
     static char[][] tablero = new char[10][10]; // (0 a n-1)
     // Fin
     // Software de gestión de venta ONLINE
-    public static void rellenarTablero(char pers){
+    public static void rellenarTablero(char pers) {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 tablero [i][j] = pers;
