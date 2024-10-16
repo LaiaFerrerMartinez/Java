8961dc1d1d40b32c1e1e97e8f6209f59ebb3fc17
LaiaFerrerMartinez <laia.ferrer.martinez@gmail.com>
2024-10-16 09:53:01 +0200

    Add files via upload

---
+++ Entornos/Ejercicio 3 - V2.0/src/EjercicioEnJava.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class EjercicioEnJava {
+
+    public static void main(String[] args) {
+        int [] vector = new int [100];
+        boolean encontrado;
+        Random r = new Random();
+        for (int i = 0; i < 100; i++) {
+            vector [i] = r.nextInt(100);
+        }
+        System.out.println("Dime un número.");
+        Scanner lector = new Scanner (System.in);
+        int num = Integer.parseInt(lector.nextLine());
+        encontrado = false;
+        for (int i = 0; i < 100; i++) {
+            if (vector[i] == num){
+                encontrado = true;
+                System.out.println("El número está en la posición " + i + ".");
+            }
+        }
+        if (!encontrado) {
+            System.out.println("El número no está en el vector.");
+        }
+    }
+}
