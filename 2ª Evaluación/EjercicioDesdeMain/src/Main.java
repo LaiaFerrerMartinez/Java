aab42f960e18ec5e449ec3dacc06f83440e5a50e
Laia Ferrer Martínez <laia.ferrer.martinez@gmail.com>
2025-01-14 11:36:24 +0100

    Add files via upload

---
+++ 2ª Evaluación/EjercicioDesdeMain/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Producto magdalena1 = new Producto("Chocolate", 1.5, 5);
+        Producto magdalena2 = new Producto("Vainilla", 1.5, 5);
+        Producto magdalena3 = new Producto("Fresa", 1.5, 5);
+
+    }
+}
---
+++ 2ª Evaluación/EjercicioDesdeMain/src/Producto.java
@@ -0,0 +1,11 @@
+public class Producto {
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto (String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
