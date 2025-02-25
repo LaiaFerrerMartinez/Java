126f38d49d510ec94572d54895d71860dc265812
Laia Ferrer Martínez <laia.ferrer.martinez@gmail.com>
2025-02-25 13:35:42 +0100

    Add files via upload

--- 2ª Evaluación/09 - Superheroes/Superheroes/src/Main.java
+++ 2ª Evaluación/09 - Superheroes/Superheroes/src/Main.java
@@ -4,18 +4,18 @@ import pojo.SuperHero;
 public class Main {
     public static void main(String[] args) {
         SuperHero ironMan = Factory.createHero("IronMan", "Tony Stark");
-        ironMan.attack();  // Calls IronMan's specific attack
-        ironMan.defend();  // Calls IronMan's specific defense
+        ironMan.attack();
+        ironMan.defend();
         System.out.println();
 
         SuperHero spiderMan = Factory.createHero("SpiderMan", "Peter Parker");
-        spiderMan.attack();  // Calls SpiderMan's specific attack
-        spiderMan.defend();  // Calls SpiderMan's specific defense
+        spiderMan.attack();
+        spiderMan.defend();
         System.out.println();
 
         SuperHero hulk = Factory.createHero("Hulk", "Bruce Banner");
-        hulk.attack();  // Calls Hulk's specific attack
-        hulk.defend();  // Calls Hulk's specific defense
+        hulk.attack();
+        hulk.defend();
         System.out.println();
     }
 }
